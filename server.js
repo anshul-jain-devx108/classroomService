9e95daceb7967ed117ed9ee4b695be6b3d6fb72f
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-03-15 22:27:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.port || 5001;
 
 // Start the server with error handling
 app.listen(PORT, "0.0.0.0",() => {
