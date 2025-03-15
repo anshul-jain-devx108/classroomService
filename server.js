301dfa12161539f4f39726a2acc02741ec50f38c
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-03-15 22:24:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 5001;
 
 // Start the server with error handling
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 }).on("error", (err) => {
   console.error("❌ Server failed to start:", err);
